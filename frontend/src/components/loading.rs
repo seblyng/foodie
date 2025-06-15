af266579384959d5bf2052fc373fedb89e6a5170
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-06-15 17:20:41 +0200

    fix: spinner loading

--- frontend/src/components/loading.rs
+++ frontend/src/components/loading.rs
@@ -1,10 +1,11 @@
 use leptos::prelude::*;
+use thaw::Spinner;
 
 #[component]
 pub fn Loading() -> impl IntoView {
     view! {
         <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
-            <span class="loading loading-spinner loading-lg"></span>
+            <Spinner />
         </div>
     }
 }
